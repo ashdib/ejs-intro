62a50fab4388c3ba3936308dfe139c132732104a
axeladib <nabil298adib@gmail.com>
2024-01-27 22:31:17 +0800

    Angela solution

--- index.js
+++ index.js
@@ -5,25 +5,36 @@ const port = 3000;
 // get current day and turn into index
 // if index is between 0 to 5 then is weekday
 // else the day is weekend
-const indexDay = new Date().getDay();
+// const indexDay = new Date().getDay();
 // check if statusOfDay
 // if index is 0 or 5 is weekend
 // other than that is weekdays
 
-const statusDay = (indexOfDay) => {
-  if (indexOfDay === 0 || indexOfDay === 6) {
-    return "This is weekend";
-  } else {
-    return "This is weekday, work hard fellas";
-  }
-};
+// const statusDay = (indexOfDay) => {
+//   if (indexOfDay === 0 || indexOfDay === 6) {
+//     return "This is weekend";
+//   } else {
+//     return "This is weekday, work hard fellas";
+//   }
+// };
 
-var getStatusDay = statusDay(indexDay);
+// var getStatusDay = statusDay(indexDay);
 
 app.get("/", (req, res) => {
+  const today = new Date();
+  const day = today.getDay();
+
+  let type = "a weekday";
+  let adv = "it's time to work hard";
+
+  if(day===0 || day === 6){
+    type = "a weekend";
+    adv =  "it's time to have some fun";
+  }
+
   res.render("index.ejs", {
-    dayType: getStatusDay,
-    advice: "Here is the result",
+    dayType: type,
+    advice: adv,
   });
 });
 
